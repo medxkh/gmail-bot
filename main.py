1a56f303dde44000401b673c8a9c693fb8968432
medxkh <medkh6699@gmail.com>
2020-12-31 13:42:20 +0100

    add speech recognition

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import smtplib
 import speech_recognition as ar
+import pyttsx3
 
 listeer = sr.Recognizer()
+engine = pyttsx3.init()
 
-try:
-    with sr.Microphone() as source:
-        print('listening...')
-        voice = listener.listen(source)
-        info = listener.recognize_google(voice)
-        print(info)
-except :  
-    pass       
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def get_info():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            info = listener.recognize_google(voice)
+            print(info)
+            return info.lower()
+    except :  
+        pass       
 
 def send_email():
     server = smtplib.SMTP('smtp.gmail.com',587)
@@ -21,3 +30,8 @@ def send_email():
     'medkh6699@gmail.com',
     'hi dude'
     )
+
+def get_email_info():
+    talk('To whom you want to send email')
+
+get_email_info()    
